9868f94a0f733b960c556477864baf1ca24677db
julien barreau <julien.barreau@developpement-logiciel.fr>
2014-05-22 17:11:31 +0200

    adding last functions to IAction

--- src/interfaces/IAction.java
+++ src/interfaces/IAction.java
@@ -1,14 +1,19 @@
 package interfaces;
 
 public interface IAction {
-	//moving functions
+	//  moving functions
 	public void goUp();
 	public void goDown();
 	public void goRight();
 	public void goLeft();
 	
-	//boxes functions
+	//  boxes functions
 	//public Box takeBox(Direction from);
 	public void takeBox();
 	public void putDownBox(); 
+	
+	//  fun functions
+	public void killMyself();
+	//public Robot createRobot();
+	public void createRobot();
 }
